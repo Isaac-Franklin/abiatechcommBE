4c58a51a0c70d2c27574793a3b3fd52a02bb7964
unknown <nwosuemmanuel159@gmail.com>
2026-01-25 01:35:17 +0100

    comiited

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,8 @@
 import os
 import dj_database_url
 from .base import *
+from datetime import timedelta
+
 DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL"),
