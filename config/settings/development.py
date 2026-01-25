f8b8145272d69095cf153e5c4b3330c584f2fa23
unknown <nwosuemmanuel159@gmail.com>
2026-01-25 07:24:03 +0100

    add to CORS_ALLOWED_ORIGINS

--- config/settings/development.py
+++ config/settings/development.py
@@ -36,8 +36,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [,
     "http://localhost:8000",
+    "http://localhost:8080",
     "http://localhost:3000",
     "https://winxnovel.netlify.app", 
 ]
