22a87e51b5b8d5407a3ca51b1fc7b80ad946704a
unknown <nwosuemmanuel159@gmail.com>
2026-01-30 18:52:26 +0100

    latest commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,24 +3,19 @@ import os
 from .base import *
 from datetime import timedelta
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'nwoemma$default',  # Format: username$dbname
-#         'USER': 'nwoemma',
-#         'PASSWORD': 'Emmy77@gmail.com',  # Set this from Database tab
-#         'HOST': 'nwoemma.mysql.pythonanywhere-services.com',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nwoemma$default',  # Format: username$dbname
+        'USER': 'nwoemma',
+        'PASSWORD': 'Emmy77@gmail.com',  # Set this from Database tab
+        'HOST': 'nwoemma.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 ALLOWED_HOSTS = ["nwoemma.pythonanywhere.com","127.0.0.1"]
 DEBUG =False
 
